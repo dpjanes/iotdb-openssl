18bae5cb2f1cdce32691802bb1978f254599cc20
David Janes <davidjanes@davidjanes.com>
2017-08-07 19:44:58 -0400

    rough in some ideas

--- index.js
+++ index.js
@@ -22,3 +22,86 @@
 
 "use strict";
 
+const _ = require("iotdb-helpers")
+
+const bluebird = require("bluebird")
+const Q = require("bluebird-q")
+const openssl = require("openssl-wrapper").exec
+const tmp = require('tmp');
+
+
+const fs = require("fs")
+const assert = require("assert")
+
+/**
+ *  This will look up data in self and write
+ *  it to a file. The filename is returned
+ */
+const include = keypath => self => {
+    const buffer = _.d.first(self, keypath)
+    assert.ok(_.is.Buffer(buffer))
+
+    const t = tmp.fileSync();
+
+    fs.writeFileSync(t.name, buffer)
+
+    return t.name;
+}
+
+/**
+ */
+const _build = (name, command, post) => {
+    command = command || name;
+    post = post || (s => s);
+
+    return Q.denodeify(
+        (_self, done) => {
+            const self = _.d.clone.shallow(_self)
+            const in_key = `${name}_in`
+            const out_key = `${name}_out`
+
+            const _inclusions = o => {
+                if (_.is.Array(o)) {
+                    return o.map(_inclusions);
+                } else if (_.is.Function(o)) {
+                    return o(self)
+                } else if (_.is.Object(o)) {
+                    return _.mapObject(o, (value, key) => _inclusions(value))
+                } else {
+                    return o;
+                }
+            }
+
+
+            const options = _inclusions(self[in_key])
+
+            // trailing parameters come in with "_"
+            if (_.is.Nullish(options._)) {
+            } else if (_.is.Array(options._)) {
+                options._.forEach(last => options[last] = false)
+            } else {
+                options[options._] = false;
+            }
+            delete options._;
+
+            openssl(command, options, (error, out) => {
+                if (error) { 
+                    return done(error)
+                }
+
+                self.document = out;
+                post(self)
+
+                done(null, self)
+            })
+        }
+    )
+}
+
+/**
+ *  API
+ */
+exports.x509 = _build("x509")
+exports.dgst = _build("dgst")
+exports.dgst.verify = _build("dgst", null, self => { self.verified = self.document.toString() === "Verified OK\n" ? true : false }) 
+exports.include = include;
