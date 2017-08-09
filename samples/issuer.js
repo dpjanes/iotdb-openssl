2e9747500dfceaf2ae4dda151b7b91499266ea64
David Janes <davidjanes@davidjanes.com>
2017-08-09 07:34:35 -0400

    some sample files

---
+++ samples/issuer.js
@@ -0,0 +1,29 @@
+/*
+ *  issuer.js
+ *
+ *  David Janes
+ *  IOTDB
+ *  2017-08-09
+ *
+ *  This demonstrates how to pull the issuer out
+ *  of a file. See "issuer_native" for a better 
+ *  option.
+ */
+const _ = require("iotdb-helpers")
+const Q = require("bluebird-q")
+
+const openssl = require("iotdb-openssl")
+
+Q({
+    x509_in: {
+        in: "key.crt",
+        issuer: true,
+    },
+})
+    .then(openssl.x509)
+    .then(sd => {
+        console.log(sd.document.toString("utf-8").split("\n")[0])
+    })
+    .catch(error => {
+        console.log(error);
+    })
---
+++ samples/issuer_native.js
@@ -0,0 +1,31 @@
+/*
+ *  issuer.js
+ *
+ *  David Janes
+ *  IOTDB
+ *  2017-08-09
+ *
+ *  This demonstrates how to pull the issuer out
+ *  of a file. See "issuer_native" for a better 
+ *  option.
+ */
+
+const _ = require("iotdb-helpers")
+
+const fs = require("iotdb-fs")
+
+const Q = require("bluebird-q")
+const x509 = require("x509");
+
+Q({
+    path: "key.crt",
+})
+    .then(fs.read.utf8)
+    .then(sd => {
+        // const issuer = x509.parseCert(__dirname + "/key.crt")
+        const issuer = x509.getIssuer(sd.document)
+        console.log(JSON.stringify(issuer, null, 2))
+    })
+    .catch(error => {
+        console.log("#", error)
+    })
